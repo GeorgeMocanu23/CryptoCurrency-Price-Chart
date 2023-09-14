f59d647d0b3b9198462205649088e38bce6b603d
George Mocanu <124595318+GeorgeMocanu23@users.noreply.github.com>
2023-09-14 15:36:14 +0200

    Update Chart.jsx
    I've added some Material UI

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,60 +1,78 @@
 import React, { useState, useEffect } from 'react';
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip
+} from 'recharts';
+import {
+  Container,
+  Card,
+  CardContent,
+  Typography,
+  Box
+} from '@mui/material';
 
 function Chart(props) {
-    const [coinData, setCoinData] = useState([]);
-    const [coinLogo, setCoinLogo] = useState('');
-    const [formattedStartDate, setFormattedStartDate] = useState('');
-    const [formattedEndDate, setFormattedEndDate] = useState('');
+  const [coinData, setCoinData] = useState([]);
+  const [coinLogo, setCoinLogo] = useState('');
+  const [formattedStartDate, setFormattedStartDate] = useState('');
+  const [formattedEndDate, setFormattedEndDate] = useState('');
 
-    useEffect(() => {
-        if (props.selectedCurrency && props.selectedDateRange) {
-            const startDate = props.selectedDateRange[0].getTime() / 1000;
-            const endDate = props.selectedDateRange[1].getTime() / 1000;
-            let apiUrl = `https://api.coingecko.com/api/v3/coins/${props.selectedCurrency}/market_chart/range?vs_currency=usd&from=${startDate}&to=${endDate}`;
-            fetch(apiUrl)
-                .then((response) => response.json())
-                .then((data) => {
-                    const formattedData = data.prices.map(([timestamp, price]) => ({
-                        date: new Date(timestamp).toLocaleDateString('en-GB'),
-                        price: price
-                }));
-                setCoinData(formattedData);
-                let apiLogoUrl = `https://api.coingecko.com/api/v3/coins/${props.selectedCurrency}`;
-                fetch(apiLogoUrl)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        setCoinLogo(data.image.small);
-                        setFormattedStartDate(new Date(startDate * 1000).toLocaleDateString('en-GB'));
-                        setFormattedEndDate(new Date().toLocaleDateString('en-GB'));
-                    })
-                    .catch((error) => console.log(error));
-                })
+  useEffect(() => {
+    if (props.selectedCurrency && props.selectedDateRange) {
+      const startDate = props.selectedDateRange[0].getTime() / 1000;
+      const endDate = props.selectedDateRange[1].getTime() / 1000;
+      let apiUrl = `https://api.coingecko.com/api/v3/coins/${props.selectedCurrency}/market_chart/range?vs_currency=usd&from=${startDate}&to=${endDate}`;
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          const formattedData = data.prices.map(([timestamp, price]) => ({
+            date: new Date(timestamp).toLocaleDateString('en-GB'),
+            price: price
+          }));
+          setCoinData(formattedData);
+          let apiLogoUrl = `https://api.coingecko.com/api/v3/coins/${props.selectedCurrency}`;
+          fetch(apiLogoUrl)
+            .then((response) => response.json())
+            .then((data) => {
+              setCoinLogo(data.image.small);
+              setFormattedStartDate(new Date(startDate * 1000).toLocaleDateString('en-GB'));
+              setFormattedEndDate(new Date().toLocaleDateString('en-GB'));
+            })
             .catch((error) => console.log(error));
-        }
-    }, [props.selectedCurrency, props.selectedDateRange]);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [props.selectedCurrency, props.selectedDateRange]);
 
-    return (
-        <div>
-            {coinData.length > 0 && (
-                <div>
-                    <h4>{`${props.selectedCurrency.toUpperCase()} prices from ${formattedStartDate} to ${formattedEndDate}`}</h4>
-                    {coinLogo && <img src={coinLogo} alt={`${props.selectedCurrency} logo`} />}
-                    <ResponsiveContainer width="100%" height={300}>
-                        <LineChart data={coinData}>
-                            <XAxis dataKey="date" />
-                            <YAxis dataKey="price" />
-                            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                            <Tooltip
-                                formatter={(value) => [`Price: ${value.toFixed(4)} $`]}
-                            />
-                            <Line type="monotone" dataKey="price" stroke="#0eaeee" strokeWidth={2} dot={false}/>
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <Container maxWidth="md">
+      <Box mt={2} />
+      {coinData.length > 0 && (
+        <Card>
+          <CardContent>
+            <Typography variant="h6">
+              {`${props.selectedCurrency.toUpperCase()} prices from ${formattedStartDate} to ${formattedEndDate}`}
+            </Typography>
+            {coinLogo && <img src={coinLogo} alt={`${props.selectedCurrency} logo`} />}
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart data={coinData}>
+                <XAxis dataKey="date" />
+                <YAxis dataKey="price" />
+                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                <Tooltip formatter={(value) => [`Price: ${value.toFixed(4)} $`]} />
+                <Line type="monotone" dataKey="price" stroke="#0eaeee" strokeWidth={2} dot={false} />
+              </LineChart>
+            </ResponsiveContainer>
+          </CardContent>
+        </Card>
+      )}
+    </Container>
+  );
 }
 
 export default Chart;
