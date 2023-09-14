1757c16d20b7eab02fef2249ff325b6d67786b8f
George Mocanu <124595318+GeorgeMocanu23@users.noreply.github.com>
2023-09-14 15:38:00 +0200

    Update DataSelect.jsx
    I've added some Material UI

--- src/DataSelect.jsx
+++ src/DataSelect.jsx
@@ -1,16 +1,25 @@
+import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
 function DataSelect({ selectedData, handleDataChange }) {
-    return (
-        <div>
-            <label htmlFor="data">Select a period: </label>
-            <select id="data" name="data" value={selectedData} onChange={handleDataChange}>
-                <option value={0}>Please choose the period</option>
-                <option value={1}>One day</option>
-                <option value={7}>One week</option>
-                <option value={30}>One month</option>
-                <option value={365}>One year</option>
-            </select>
-        </div>
-    );
+  return (
+    <div>
+      <FormControl fullWidth>
+        <InputLabel htmlFor="data">Select a period:</InputLabel>
+        <Select
+          id="data"
+          value={selectedData}
+          onChange={handleDataChange}
+          label="Select a period:"
+        >
+          <MenuItem value={1}>One day</MenuItem>
+          <MenuItem value={7}>One week</MenuItem>
+          <MenuItem value={30}>One month</MenuItem>
+          <MenuItem value={365}>One year</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
 
 export default DataSelect;
