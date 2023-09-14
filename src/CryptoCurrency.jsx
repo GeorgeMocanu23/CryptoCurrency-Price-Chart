be38267177fcccac71980fb4a76749783da1ae70
George Mocanu <124595318+GeorgeMocanu23@users.noreply.github.com>
2023-09-14 15:37:01 +0200

    Update CryptoCurrency.jsx
    I've added some Material UI

--- src/CryptoCurrency.jsx
+++ src/CryptoCurrency.jsx
@@ -1,47 +1,56 @@
 import React, { useState } from 'react';
+import { Container, Typography, Paper, Grid, Box } from '@mui/material';
 import Chart from './Chart';
 import CurrencySelect from './CurrencySelect';
 import DataSelect from './DataSelect';
 
 function CryptoCurrency() {
-    const [selectedCurrency, setSelectedCurrency] = useState("");
-    const [selectedData, setSelectedData] = useState("");
-    const [selectedDateRange, setSelectedDateRange] = useState("");
+  const [selectedCurrency, setSelectedCurrency] = useState("");
+  const [selectedData, setSelectedData] = useState("");
+  const [selectedDateRange, setSelectedDateRange] = useState("");
 
-    const handleCurrencyChange = (event) => {
-        const currency = event.target.value;
-        setSelectedCurrency(currency);
-    };
+  const handleCurrencyChange = (event) => {
+    const currency = event.target.value;
+    setSelectedCurrency(currency);
+  };
 
-    const handleDataChange = (event) => {
-        const days = event.target.value;
-        const endDate = new Date();
-        const startDate = new Date();
-        startDate.setDate(endDate.getDate() - days);
-        setSelectedData(days);
-        setSelectedDateRange([startDate, endDate]);
-    };
+  const handleDataChange = (event) => {
+    const days = event.target.value;
+    const endDate = new Date();
+    const startDate = new Date();
+    startDate.setDate(endDate.getDate() - days);
+    setSelectedData(days);
+    setSelectedDateRange([startDate, endDate]);
+  };
 
-    return (
-        <div>
-            <h1>CryptoCurrency App</h1>
-            <form>
-                <DataSelect
-                    selectedData={selectedData}
-                    handleDataChange={handleDataChange}
-                />
-                <br />
-                <CurrencySelect
-                    selectedCurrency={selectedCurrency}
-                    handleCurrencyChange={handleCurrencyChange}
-                />
-            </form>
-            <Chart
+  return (
+    <Container maxWidth="md" align="center">
+      <Typography variant="h3">CryptoCurrency App</Typography>
+      <Box mt={2} />
+      <Paper elevation={3} style={{ padding: '16px' }}>
+        <form>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <DataSelect
+                selectedData={selectedData}
+                handleDataChange={handleDataChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <CurrencySelect
                 selectedCurrency={selectedCurrency}
-                selectedDateRange={selectedDateRange}
-            />
-        </div>
-    );
+                handleCurrencyChange={handleCurrencyChange}
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+      <Chart
+        selectedCurrency={selectedCurrency}
+        selectedDateRange={selectedDateRange}
+      />
+    </Container>
+  );
 }
 
 export default CryptoCurrency;
